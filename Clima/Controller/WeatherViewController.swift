fa294e3d509f8e7a600048628a2e52597c9a3d96
furkannyildirimm <furkannyildirimm@hotmail.com>
2023-06-22 00:25:59 +0300

    update app.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -9,16 +9,63 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
-
-
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        
+        print(searchTextField.text)
+    }
+    
 }
+
+extension WeatherViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+}
+
+
+//switch conditionID {
+//        case 200...232:
+//            return "cloud.bolt"
+//        case 300...321:
+//            return "cloud.drizzle"
+//        case 500...531:
+//            return "cloud.rain"
+//        case 600...622:
+//            return "cloud.snow"
+//        case 701...781:
+//            return "cloud.fog"
+//        case 800:
+//            return "sun.max"
+//        case 801...804:
+//            return "cloud.bolt"
+//        default:
+//            return "cloud"
+//        }
 
